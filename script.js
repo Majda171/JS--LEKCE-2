4903e0a160c2bebe2539eb96fc8e7667f0eba4f1
Magdaléna Škobrtalová <mahdalova9@seznam.cz>
2024-09-30 20:09:17 +0200

    první příklad

---
+++ script.js
@@ -0,0 +1,10 @@
+const price = 12;
+const visitors = 174;
+const show = 15;
+const revenue = (price * visitors) * show;
+const studentPrice = 12 * 0.65;
+const students = 174 * 0.4;
+const adults = 174 - students;
+const revenueDiscount = (studentPrice * students) + (adults * price);
+document.body.innerHTML += `<p> Základní příjem divadla je ${revenue}</p>`;
+document.body.innerHTML += `<p> Příjem divadla se slevou pro studenty je ${revenueDiscount}</p>`;
