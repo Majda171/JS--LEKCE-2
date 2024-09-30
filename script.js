06a051138ce82389d0ab62120e13e645538e2b9a
Magdaléna Škobrtalová <mahdalova9@seznam.cz>
2024-09-30 20:12:55 +0200

    Druhý příklad

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ const adults = 174 - students;
 const revenueDiscount = (studentPrice * students) + (adults * price);
 document.body.innerHTML += `<p> Základní příjem divadla je ${revenue}</p>`;
 document.body.innerHTML += `<p> Příjem divadla se slevou pro studenty je ${revenueDiscount}</p>`;
+
+document.body.innerHTML += `<h1>Druhý příklad</h1>`;
+
+function rollDice() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
